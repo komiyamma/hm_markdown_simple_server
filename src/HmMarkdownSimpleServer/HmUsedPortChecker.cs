cb9e0ec305d91e2bcc230fb58d50cc3454151da7
komiyamma <komiyama0629@gmail.com>
2023-05-09 17:31:21 +0900

    try で囲ったが挙動に変化はなし。どの道１つ上のtryでキャッチされていたかどうかだけ。ポート番号に空きがない時に明示的にエラーメッセージを出すようにした。

--- src/HmMarkdownSimpleServer/HmUsedPortChecker.cs
+++ src/HmMarkdownSimpleServer/HmUsedPortChecker.cs
@@ -5,6 +5,7 @@ using System.Net.NetworkInformation;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using HmNetCOM;
 
 namespace HmMarkdownSimpleServer;
 
@@ -13,17 +14,23 @@ internal class HmUsedPortChecker
     static List<int> portsInUse;
     public static int GetAvailablePort(int beginPort, int endPort)
     {
-        var ipGP = IPGlobalProperties.GetIPGlobalProperties();
-        var tcpEPs = ipGP.GetActiveTcpListeners();
-        var udpEPs = ipGP.GetActiveUdpListeners();
-        portsInUse = tcpEPs.Concat(udpEPs).Select(p => p.Port).ToList();
-
-        for (int port = beginPort; port <= endPort; ++port)
+        try
         {
-            if (!portsInUse.Contains(port))
+            var ipGP = IPGlobalProperties.GetIPGlobalProperties();
+            var tcpEPs = ipGP.GetActiveTcpListeners();
+            var udpEPs = ipGP.GetActiveUdpListeners();
+            portsInUse = tcpEPs.Concat(udpEPs).Select(p => p.Port).ToList();
+
+            for (int port = beginPort; port <= endPort; ++port)
             {
-                return port;
+                if (!portsInUse.Contains(port))
+                {
+                    return port;
+                }
             }
+        } catch(Exception ex)
+        {
+            Hm.OutputPane.Output(ex.Message + "\r\n");
         }
 
         return 0; // 空きポートが見つからない場合
