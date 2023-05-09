bbf0cf1dba458a807f9465a6ff8f797d1103586a
komiyamma <komiyama0629@gmail.com>
2023-05-09 15:16:03 +0900

    コメント１文字変更しただけ。

--- src/HmMarkdownSimpleServer/bin/Release/HmMarkdownSimpleServerAsync.js
+++ src/HmMarkdownSimpleServer/bin/Release/HmMarkdownSimpleServerAsync.js
@@ -149,7 +149,7 @@ function isNotDetectedOperation() {
     ○ 0x00004000 中ボタンによるオートスクロール中
     ○ 0x00008000 キーやマウスの操作直後
     ○ 0x00010000 何かマウスのボタンを押している
-    ○ 0x00020000 マウスキャプチャ状態(ドラッグ状態)
+    × 0x00020000 マウスキャプチャ状態(ドラッグ状態)
     ○ 0x00040000 Hidemaru_CheckQueueStatus相当
     */
     let istatus = hidemaru.getInputStates();
