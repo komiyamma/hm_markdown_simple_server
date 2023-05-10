d1079d70eaed61c50c57a1e8231c76c9c2829b32
komiyamma <komiyama0629@gmail.com>
2023-05-10 16:33:06 +0900

    マクロの先頭にhidemaruversion を追加

--- src/HmMarkdownSimpleServer/bin/Release/HmMarkdownSimpleServerAsync.js
+++ src/HmMarkdownSimpleServer/bin/Release/HmMarkdownSimpleServerAsync.js
@@ -1,6 +1,6 @@
 /// <reference path="types/hm_jsmode.d.ts" />
 /*
- * HmMarkdownSimpleServer v1.2.0.6
+ * HmMarkdownSimpleServer v1.2.0.7
  *
  * Copyright (c) 2023 Akitsugu Komiyama
  * under the MIT License
